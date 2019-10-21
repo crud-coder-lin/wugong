8573db72cd662d8cca62fe803dabb78f366d358e
lindaxia <hello_lin2019@163.com>
2019-10-22 00:06:26 +0800

    跟新四

--- src/wugong/dugujiujian.java
+++ src/wugong/dugujiujian.java
@@ -7,6 +7,8 @@ public class dugujiujian {
 		System.out.println("6666666666");
 		System.out.println("7777777777");
 		System.out.println("8888888888");
+		System.out.println("4444444444");
+		System.out.println("4444444441");
 
 	}
 
